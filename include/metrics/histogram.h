55e7713d002c1c5b3f16615c329a1aa8ebf9a108
DarkWanderer <elquentaro@gmail.com>
2023-05-26 21:21:50 +0200

    Removed summary header for now, improved histogram const correctness

--- include/metrics/histogram.h
+++ include/metrics/histogram.h
@@ -4,6 +4,7 @@
 #include <metrics/metric.h>
 
 #include <memory>
+#include <vector>
 
 namespace Metrics
 {
@@ -13,6 +14,7 @@ namespace Metrics
         virtual void observe(double value) = 0;
         virtual uint64_t count() = 0;
         virtual double sum() = 0;
+        virtual std::vector<std::pair<double, uint64_t>> values() = 0;
 
     protected:
         METRICS_EXPORT virtual ~IHistogram() = 0;
@@ -28,6 +30,7 @@ namespace Metrics
         void observe(double value) override { m_value->observe(value); };
         uint64_t count() override { return m_value->count(); };
         double sum() override { return m_value->sum(); };
+        std::vector<std::pair<double, uint64_t>> values() override { return m_value->values(); };
 
         Histogram(std::shared_ptr<IHistogram> value) : m_value(value) {};
         Histogram(const Histogram& other) = default;
