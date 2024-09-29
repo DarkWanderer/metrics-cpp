564799f8ce7a65fe3cf124aa8f938ea9c1327789
Oleg V. Kozlyuk <elquentaro@gmail.com>
2024-09-29 15:39:26 +0200

    Switched to vcpkg (#66)
    * Switched to vcpkg, disabled Prometheus Push Gateway for now
    
    * CMake fix on Linux
    
    * Added vcpkg stuff in workflow
    
    * Moved vcpkg toolchain to all presets
    
    * Explicitly call vpckg install in Linux
    
    * Try another variant as well
    
    * Syntax fix
    
    * Fix case
    
    * Fix & reformat workflows
    
    * Fix integration test
    
    * Remove unneeded variable

--- include/metrics/prometheus.h
+++ include/metrics/prometheus.h
@@ -8,7 +8,7 @@
 namespace Metrics {
     namespace Prometheus {
         METRICS_EXPORT std::string serialize(std::shared_ptr<IRegistry> registry);
-        METRICS_EXPORT std::shared_ptr<IOnDemandSink> createPushGatewaySink(const std::string& host, const std::string& port, std::string job, std::string instance);
+        METRICS_EXPORT std::shared_ptr<IOnDemandSink> createPushGatewaySink(const std::string& url);
         METRICS_EXPORT std::shared_ptr<IRegistrySink> createPrometheusHttpServerSink(std::shared_ptr<IRegistry> registry, const std::string& address, const std::string& port);
     }
 }
