fc00f9428461f18c832dbf71a328c7d0e4efb542
Oleg V. Kozlyuk <elquentaro@gmail.com>
2025-01-03 20:53:13 +0100

    Added multithreaded benchmarks (#72)

--- benchmarks/benchmarks.cpp
+++ benchmarks/benchmarks.cpp
@@ -2,72 +2,86 @@
 #include <metrics/registry.h>
 
 #include <atomic>
+#include <thread>
 
 using namespace Metrics;
 
+static const size_t maxThreads = std::thread::hardware_concurrency();
+
 static void BM_Reference_AtomicIncrement(benchmark::State& state) {
-    std::atomic<int> counter;
+    static std::atomic<int> counter;
     for (auto _ : state)
         counter++;
+    state.SetItemsProcessed(counter.load());
 }
-BENCHMARK(BM_Reference_AtomicIncrement);
+BENCHMARK(BM_Reference_AtomicIncrement)->ThreadRange(1, maxThreads)->UseRealTime();
 
 static void BM_CounterIncrement(benchmark::State& state) {
-    auto counter = Counter();
+    static auto counter = Counter();
     for (auto _ : state)
         counter++;
+    state.SetItemsProcessed(counter.value());
 }
-BENCHMARK(BM_CounterIncrement);
+BENCHMARK(BM_CounterIncrement)->ThreadRange(1, maxThreads)->UseRealTime();
 
 static void BM_GaugeSet(benchmark::State& state) {
-    auto gauge = Gauge();
+    static auto gauge = Gauge();
     for (auto _ : state)
-        gauge = 123;
+        gauge = 1;
 }
-BENCHMARK(BM_GaugeSet);
+BENCHMARK(BM_GaugeSet)->ThreadRange(1, maxThreads)->UseRealTime();
 
-static void BM_Histogram2Observe(benchmark::State& state) {
-    auto histogram = makeHistogram({ 1,2 });
+static void BM_GaugeIncrement(benchmark::State& state) {
+    static auto gauge = Gauge();
     for (auto _ : state)
-        histogram->observe(20);
+        gauge += 1;
+    state.SetItemsProcessed(gauge.value());
 }
-BENCHMARK(BM_Histogram2Observe);
+BENCHMARK(BM_GaugeIncrement)->ThreadRange(1, maxThreads)->UseRealTime();
 
-static void BM_Histogram5Observe(benchmark::State& state) {
-    auto histogram = makeHistogram({ 1,2,3,4,5 });
-    for (auto _ : state)
-        histogram->observe(20);
-}
-BENCHMARK(BM_Histogram5Observe);
+static void BM_HistogramObserve(benchmark::State& state) {
+    static std::shared_ptr<IHistogram> histogram;
 
-static void BM_Histogram10Observe(benchmark::State& state) {
-    auto histogram = makeHistogram({ 1,2,3,4,5,6,7,8,9,10 });
+    if (state.thread_index() == 0) {
+        int nBuckets = state.range(0);
+        std::vector<double> buckets;
+        buckets.reserve(nBuckets);
+
+        for (int i = 0; i < nBuckets; i++)
+            buckets.push_back(i+1);
+
+        histogram = makeHistogram(buckets);
+    }
+
     for (auto _ : state)
-        histogram->observe(20);
+        histogram->observe(5);
+
+    state.SetItemsProcessed(histogram->count());
 }
-BENCHMARK(BM_Histogram10Observe);
+BENCHMARK(BM_HistogramObserve)->Range(2, 5)->ThreadRange(1, maxThreads)->UseRealTime();
 
 static void BM_SummaryObserve(benchmark::State& state) {
-    auto summary = makeSummary({ 0.9, 0.99, 0.999 }, 0.01);
+    static auto summary = makeSummary({ 0.9, 0.99, 0.999 }, 0.01);
     for (auto _ : state)
         summary->observe(5);
+    state.SetItemsProcessed(summary->count());
 }
-BENCHMARK(BM_SummaryObserve);
+BENCHMARK(BM_SummaryObserve)->ThreadRange(1, maxThreads)->UseRealTime();
 
 static void BM_RegistryGet(benchmark::State& state) {
-    auto registry = createRegistry();
+    static auto registry = createRegistry();
     registry->getCounter("test");
     for (auto _ : state)
         registry->getCounter("test");
 }
-BENCHMARK(BM_RegistryGet);
+BENCHMARK(BM_RegistryGet)->ThreadRange(1, maxThreads)->UseRealTime();
 
 static void BM_RegistryGetLabels(benchmark::State& state) {
-    auto registry = createRegistry();
+    static auto registry = createRegistry();
     registry->getCounter("test", { {"a", "b"} });
     for (auto _ : state)
         registry->getCounter("test", { {"a", "b"} });
 }
-BENCHMARK(BM_RegistryGetLabels);
+BENCHMARK(BM_RegistryGetLabels)->ThreadRange(1, maxThreads)->UseRealTime();
 
 BENCHMARK_MAIN();
